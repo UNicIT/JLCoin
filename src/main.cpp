4206e6b18f59a9fe9c304d5749dbeb4268a231da
Jonas Lambert <jonas.lambert@drakon.se>
2017-04-14 15:54:01 +0200

    Genesis block is ready

--- src/main.cpp
+++ src/main.cpp
@@ -1981,7 +1981,7 @@ bool LoadBlockIndex(bool fAllowNew)
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb8;
         pchMessageStart[3] = 0xdb;
-        hashGenesisBlock = uint256("0x5813d4cfab0eeda94a15d11c5e7d6895e667fbbe67c59ef9a1b3bc232c9a0b7f");
+        hashGenesisBlock = uint256("0x8e0e420507d8156752554f164fdfb3d7f7f37bdb4dac24a63bf6c5c45d341957");
     }
 
     //
@@ -2021,22 +2021,22 @@ bool LoadBlockIndex(bool fAllowNew)
         if (fTestNet)
         {
             block.nTime    = 1492158457;
-            block.nNonce   = 0;
+            block.nNonce   = 2808534;
         }
 
         //// debug print
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x2fc1b7ef46270c053711fbae934cf7f83378efd4b3e158079451d9c6c90e4700"));
+        assert(block.hashMerkleRoot == uint256("0x7f8cb1602788f2b8b89b2fdb3c02703188239d5f4fa2e8044c207f897c7dfc6e"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (false && block.GetHash() != hashGenesisBlock)
+        if (true && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
             // creating a different genesis block:
-            uint256 hashTarget = CBigNum().SetCompact(block.nBits).getuint256();
+            uint256 hashTarget = CBigNum().SetCompact(block.nBits).getuint256();                                                
             uint256 thash;
             char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
 
